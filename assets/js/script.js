c9fa99872df54aa9840d130a9aadca8099dd3c79
Yumika <yumika0904@live.jp>
2022-09-02 12:04:41 +0800

    attempt2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,58 @@
+// 
+//  Presented with another question
+// Time is subtracted from the clock, when answered incorrectly
+// Game is over when all questions are answered or the timer reaches 0
+// Save initials and score when game is over 
+
+// Game starts(= timer starts, questions are displayed and start_msg disappears) once start_btn has been clicked
 const startButton = document.getElementById("start_btn")
 startButton.addEventListener("click", startGame)
+
 function startGame() {
-  console.log("Started")
+  countdown()
+  hideMessage()
+  displayQuestions()
 }
+
+// Timer starts 
+var timerEl = document.getElementById("timer_sec");
+var mainEl = document.getElementById("box_content");
+var startMsgEl = document.getElementById("start_msg");
+
+function countdown() {
+  var timeLeft = 60;
+
+var timeInterval = setInterval(function () {
+  if (timeLeft === 0 ){
+    clearInterval(timeInterval)  
+  }
+  // Output timeLeft to the console to double check 
+  console.log(timeLeft)
+
+  // Show how many seconds left
+  if (timeLeft > 0) {
+    timerEl.textContent = `${timeLeft}`
+  }
+
+  //
+  else {
+    timerEl.textContent = ""
+  }
+  timeLeft = timeLeft - 1
+}, 1000);
+}
+
+//  Display question
+function displayQuestions() {
+      mainEl.style.display = "block";
+    }
+
+// Hide start_msg
+function hideMessage() {
+  startMsgEl.style.display = "none";
+}
+
+
+
+
+
